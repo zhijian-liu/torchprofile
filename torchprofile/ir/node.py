30031d9f2811be5365c578590abe11f45dc74079
Zhijian Liu <zhijianliu.cs@gmail.com>
2019-09-06 17:16:35 -0400

    [minor] add properties to `Node`

--- torchprofile/ir/node.py
+++ torchprofile/ir/node.py
@@ -3,11 +3,31 @@ __all__ = ['Node']
 
 class Node:
     def __init__(self, operator, attributes, inputs, outputs, scope):
-        self.operator = operator.lower()
-        self.attributes = attributes
-        self.inputs = inputs
-        self.outputs = outputs
-        self.scope = scope.lower()
+        self._operator = operator.lower()
+        self._attributes = attributes
+        self._inputs = inputs
+        self._outputs = outputs
+        self._scope = scope.lower()
+
+    @property
+    def operator(self):
+        return self._operator
+
+    @property
+    def attributes(self):
+        return self._attributes
+
+    @property
+    def inputs(self):
+        return self._inputs
+
+    @property
+    def outputs(self):
+        return self._outputs
+
+    @property
+    def scope(self):
+        return self._scope
 
     def __str__(self):
         return '{} = {}({})'.format(', '.join([str(tensor) for tensor in self.outputs]), self.operator,
