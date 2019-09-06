9c28d33c9f472d07cc2706467d32f50e03809791
Zhijian Liu <zhijianliu.cs@gmail.com>
2019-09-06 16:57:36 -0400

    [minor] add properties to `Tensor`

--- torchprofile/ir/tensor.py
+++ torchprofile/ir/tensor.py
@@ -3,9 +3,24 @@ __all__ = ['Tensor']
 
 class Tensor:
     def __init__(self, name, dtype, shape):
-        self.name = name
-        self.dtype = dtype.lower()
-        self.shape = shape
+        self._name = name
+        self._dtype = dtype.lower()
+        self._shape = shape
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def dtype(self):
+        return self._dtype
+
+    @property
+    def shape(self):
+        return self._shape
+
+    def size(self):
+        return self._shape
 
     def __str__(self):
         return '%{}'.format(self.name)
