8b4b981e5c120b6dfe5cb5dcbbcb427d8a6e3e00
Zhijian Liu <zhijianliu.cs@gmail.com>
2019-09-07 12:32:22 -0400

    [minor] add attribute setters for `Node`

--- torchprofile/ir/node.py
+++ torchprofile/ir/node.py
@@ -3,31 +3,51 @@ __all__ = ['Node']
 
 class Node:
     def __init__(self, operator, attributes, inputs, outputs, scope):
-        self._operator = operator.lower()
-        self._attributes = attributes
-        self._inputs = inputs
-        self._outputs = outputs
-        self._scope = scope
+        self.operator = operator
+        self.attributes = attributes
+        self.inputs = inputs
+        self.outputs = outputs
+        self.scope = scope
 
     @property
     def operator(self):
         return self._operator
 
+    @operator.setter
+    def operator(self, operator):
+        self._operator = operator.lower()
+
     @property
     def attributes(self):
         return self._attributes
+
+    @attributes.setter
+    def attributes(self, attributes):
+        self._attributes = attributes
 
     @property
     def inputs(self):
         return self._inputs
 
+    @inputs.setter
+    def inputs(self, inputs):
+        self._inputs = inputs
+
     @property
     def outputs(self):
         return self._outputs
 
+    @outputs.setter
+    def outputs(self, outputs):
+        self._outputs = outputs
+
     @property
     def scope(self):
         return self._scope
+
+    @scope.setter
+    def scope(self, scope):
+        self._scope = scope
 
     def __str__(self):
         text = ''
@@ -35,7 +55,7 @@ class Node:
             text += ', '.join([str(tensor) for tensor in self.outputs])
         text += ' = ' + self.operator
         if self.attributes:
-            text += '[' + ', '.join(['{}={}'.format(k, v) for k, v in self.attributes.items()])+ ']'
+            text += '[' + ', '.join(['{}={}'.format(k, v) for k, v in self.attributes.items()]) + ']'
         if self.inputs:
             text += '(' + ', '.join([str(tensor) for tensor in self.inputs]) + ')'
         if self.scope:
