7468a0a75dd315c6aebc8a6364578d59e1335e21
Zhijian Liu <zhijianliu.cs@gmail.com>
2019-09-07 12:11:23 -0400

    [minor] add property setters

--- torchprofile/ir/tensor.py
+++ torchprofile/ir/tensor.py
@@ -3,21 +3,33 @@ __all__ = ['Tensor']
 
 class Tensor:
     def __init__(self, name, dtype, shape):
-        self._name = name
-        self._dtype = dtype.lower()
-        self._shape = shape
+        self.name = name
+        self.dtype = dtype
+        self.shape = shape
 
     @property
     def name(self):
         return self._name
 
+    @name.setter
+    def name(self, name):
+        self._name = name
+
     @property
     def dtype(self):
         return self._dtype
 
+    @dtype.setter
+    def dtype(self, dtype):
+        self._dtype = dtype.lower()
+
     @property
     def shape(self):
         return self._shape
+
+    @shape.setter
+    def shape(self, shape):
+        self._shape = shape
 
     def size(self):
         return self._shape
