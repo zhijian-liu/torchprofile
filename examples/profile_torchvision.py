5dc9ff9bc56ae3c83c45b513c3f6db978f21a594
Zhijian Liu <zhijianliu.cs@gmail.com>
2019-09-09 19:10:06 -0400

    [major] support inception_v3 in example

--- examples/profile_torchvision.py
+++ examples/profile_torchvision.py
@@ -5,8 +5,14 @@ from torchprofile import profile_macs
 
 if __name__ == '__main__':
     for name, model in models.__dict__.items():
-        if name.islower() and not name.startswith('__') and 'inception' not in name and callable(model):
-            model = model()
+        if not name.islower() or name.startswith('__') or not callable(model):
+            continue
+
+        model = model().eval()
+        if 'inception' not in name:
             inputs = torch.randn(1, 3, 224, 224)
-            total_macs = profile_macs(model, inputs)
-            print("%s: %.2f GMACs" % (name, total_macs / 1e9))
+        else:
+            inputs = torch.randn(1, 3, 299, 299)
+
+        total_macs = profile_macs(model, inputs)
+        print('{}: {:.4g} G'.format(name, total_macs / 1e9))
