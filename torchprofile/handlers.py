bcf93d82be89e5fc740efa7463cc43c954d2782b
Zhijian Liu <zhijianliu.cs@gmail.com>
2019-09-09 21:19:40 -0400

    [minor] update handlers

--- torchprofile/handlers.py
+++ torchprofile/handlers.py
@@ -42,6 +42,10 @@ def convolution(node):
     return np.prod(node.outputs[0].shape + node.inputs[1].shape[1:])
 
 
+def batch_norm(node):
+    return 0
+
+
 def layer_norm(node):
     # [30, 1, 512] = aten::layer_norm(float[30, 1, 512], *, float[512], float[512], *, *)
     return np.prod(node.outputs[0].shape)
@@ -63,19 +67,19 @@ handlers = (
     (('aten::mul', 'aten::mul_'), mul),
 
     ('aten::_convolution', convolution),
+
+    ('aten::batch_norm', batch_norm),
     ('aten::layer_norm', layer_norm),
 
     ('aten::mean', mean),
 
     # TODO: need to be fixed
-    (('aten::add', 'aten::add_'), zero),
-    (('aten::div', 'aten::div_'), zero),
-    ('aten::max_pool2d', zero),
-    ('aten::batch_norm', zero),
-    (('aten::avg_pool2d', 'aten::adaptive_avg_pool2d'), zero),
+    ('aten::avg_pool2d', zero),
+    ('aten::adaptive_avg_pool2d', zero),
 
-    (('aten::cat', 'aten::chunk', 'aten::clone', 'aten::contiguous', 'aten::dropout', 'aten::dropout_', 'aten::eq',
-      'aten::flatten', 'aten::hardtanh_', 'aten::int', 'aten::ne', 'aten::relu', 'aten::relu_', 'aten::select',
-      'aten::size', 'aten::slice', 'aten::softmax', 'aten::sum', 'aten::t', 'aten::transpose', 'aten::view',
-      'prim::constant', 'prim::listconstruct', 'prim::listunpack', 'prim::numtotensor'), zero)
+    (('aten::add', 'aten::add_', 'aten::cat', 'aten::chunk', 'aten::clone', 'aten::contiguous', 'aten::div',
+      'aten::div_' 'aten::dropout', 'aten::dropout_', 'aten::eq', 'aten::flatten', 'aten::hardtanh_', 'aten::int',
+      'aten::max_pool1d', 'aten::max_pool2d', 'aten::max_pool3d', 'aten::ne', 'aten::relu', 'aten::relu_',
+      'aten::select', 'aten::size', 'aten::slice', 'aten::softmax', 'aten::sum', 'aten::t', 'aten::transpose',
+      'aten::view', 'prim::constant', 'prim::listconstruct', 'prim::listunpack', 'prim::numtotensor'), zero)
 )
