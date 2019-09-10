af9eab6d8ea4076ca11316f72d75421f2cfe59b9
Zhijian Liu <zhijianliu.cs@gmail.com>
2019-09-10 01:41:22 -0400

    [minor] add handlers for avg_pool

--- torchprofile/handlers.py
+++ torchprofile/handlers.py
@@ -5,34 +5,21 @@ __all__ = ['handlers']
 
 def addmm(node):
     # [n, p] = aten::addmm([n(, p)], [n, m], [m, p], *, *)
-    assert len(node.inputs) == 5 and len(node.inputs[0].shape) in [1, 2] and len(node.inputs[1].shape) == 2 and \
-           len(node.inputs[2].shape) == 2 and len(node.outputs) == 1 and len(node.outputs[0].shape) == 2, str(node)
-
-    n = node.outputs[0].shape[0]
-    p = node.outputs[0].shape[1]
-    m = node.inputs[1].shape[1]
+    n, p = node.outputs[0].shape
+    n, m = node.inputs[1].shape
     return n * m * p
 
 
 def addmv(node):
     # [n] = aten::addmv([n], [n, m], [m], *, *)
-    assert len(node.inputs) == 5 and len(node.inputs[0].shape) == 1 and len(node.inputs[1].shape) == 2 and \
-           len(node.inputs[2].shape) == 1 and len(node.outputs) == 1 and len(node.outputs[0].shape) == 1, str(node)
-
-    n = node.outputs[0].shape[0]
-    m = node.inputs[1].shape[1]
+    n, m = node.outputs[1].shape
     return n * m
 
 
 def bmm(node):
     # [b, n, p] = aten::bmm([b, n, m], [b, m, p])
-    assert len(node.inputs) == 2 and len(node.inputs[0].shape) == 3 and len(node.inputs[1].shape) == 3 and \
-           len(node.outputs) == 1 and len(node.outputs[0].shape) == 3, str(node)
-
-    b = node.outputs[0].shape[0]
-    n = node.outputs[0].shape[1]
-    p = node.outputs[0].shape[2]
-    m = node.inputs[0].shape[2]
+    b, n, p = node.outputs[0].shape
+    b, n, m = node.inputs[0].shape
     return b * n * m * p
 
 
@@ -41,14 +28,15 @@ def matmul(node):
 
 
 def mul(node):
-    # [n, m] = aten::mul([n, p], [p, m])
-    # [n, m] = aten::mul([n, m], *)
-    nm = node.outputs[0].shape
-    return np.prod(nm)
+    os = node.outputs[0].shape
+    return np.prod(os)
 
 
 def convolution(node):
-    return np.prod(node.outputs[0].shape + node.inputs[1].shape[1:])
+    os = node.outputs[0].shape
+    ic = node.inputs[1].shape[1]
+    ks = node.inputs[1].shape[2:]
+    return np.prod(os) * ic * np.prod(ks)
 
 
 def batch_norm(node):
@@ -58,12 +46,17 @@ def batch_norm(node):
 
 
 def layer_norm(node):
-    nm = node.outputs[0].shape
-    return np.prod(nm)
+    os = node.outputs[0].shape
+    return np.prod(os)
 
 
 def mean(node):
     return 1
+
+
+def avg_pool(node):
+    output_size = node.outputs[0].shape
+    return np.prod(output_size)
 
 
 def none(node):
@@ -76,21 +69,18 @@ handlers = (
     ('aten::bmm', bmm),
     ('aten::matmul', matmul),
     (('aten::mul', 'aten::mul_'), mul),
-
     ('aten::_convolution', convolution),
-
     ('aten::batch_norm', batch_norm),
     ('aten::layer_norm', layer_norm),
-
     ('aten::mean', mean),
 
-    # TODO: need to be fixed
-    ('aten::avg_pool2d', none),
-    ('aten::adaptive_avg_pool2d', none),
+    (('aten::avg_pool1d', 'aten::avg_pool2d', 'aten::avg_pool3d'), avg_pool),
+    (('aten::adaptive_avg_pool1d', 'aten::adaptive_avg_pool2d', 'aten::adaptive_avg_pool3d'), avg_pool),
 
-    (('aten::add', 'aten::add_', 'aten::cat', 'aten::chunk', 'aten::clone', 'aten::contiguous', 'aten::div',
-      'aten::div_', 'aten::dropout', 'aten::dropout_', 'aten::eq', 'aten::flatten', 'aten::hardtanh_', 'aten::int',
-      'aten::max_pool1d', 'aten::max_pool2d', 'aten::max_pool3d', 'aten::ne', 'aten::relu', 'aten::relu_',
-      'aten::select', 'aten::size', 'aten::slice', 'aten::softmax', 'aten::sum', 'aten::t', 'aten::transpose',
-      'aten::view', 'prim::constant', 'prim::listconstruct', 'prim::listunpack', 'prim::numtotensor'), none)
+    (('aten::adaptive_max_pool1d', 'aten::adaptive_max_pool2d', 'aten::adaptive_max_pool3d', 'aten::add', 'aten::add_',
+      'aten::cat', 'aten::chunk', 'aten::clone', 'aten::contiguous', 'aten::div', 'aten::div_', 'aten::dropout',
+      'aten::dropout_', 'aten::eq', 'aten::flatten', 'aten::hardtanh_', 'aten::int', 'aten::max_pool1d',
+      'aten::max_pool2d', 'aten::max_pool3d', 'aten::ne', 'aten::relu', 'aten::relu_', 'aten::select', 'aten::size',
+      'aten::slice', 'aten::softmax', 'aten::sum', 'aten::t', 'aten::transpose', 'aten::view', 'prim::constant',
+      'prim::listconstruct', 'prim::listunpack', 'prim::numtotensor'), none)
 )
