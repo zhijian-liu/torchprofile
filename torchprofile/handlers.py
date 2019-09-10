1e48711ee2b3ff148e9e77dff11773b1b5cd244b
Zhijian Liu <zhijianliu.cs@gmail.com>
2019-09-09 22:03:05 -0400

    [major] add assertions to handlers

--- torchprofile/handlers.py
+++ torchprofile/handlers.py
@@ -5,14 +5,20 @@ __all__ = ['handlers']
 
 def addmm(node):
     # [n, p] = aten::addmm([n(, p)], [n, m], [m, p], *, *)
+    assert len(node.inputs) == 5 and len(node.inputs[0].shape) in [1, 2] and len(node.inputs[1].shape) == 2 and \
+           len(node.inputs[2].shape) == 2 and len(node.outputs) == 1 and len(node.outputs[0].shape) == 2, str(node)
+
     n = node.outputs[0].shape[0]
     p = node.outputs[0].shape[1]
     m = node.inputs[1].shape[1]
-    return n * p * m
+    return n * m * p
 
 
 def addmv(node):
     # [n] = aten::addmv([n], [n, m], [m], *, *)
+    assert len(node.inputs) == 5 and len(node.inputs[0].shape) == 1 and len(node.inputs[1].shape) == 2 and \
+           len(node.inputs[2].shape) == 1 and len(node.outputs) == 1 and len(node.outputs[0].shape) == 1, str(node)
+
     n = node.outputs[0].shape[0]
     m = node.inputs[1].shape[1]
     return n * m
@@ -20,15 +26,17 @@ def addmv(node):
 
 def bmm(node):
     # [b, n, p] = aten::bmm([b, n, m], [b, m, p])
+    assert len(node.inputs) == 2 and len(node.inputs[0].shape) == 3 and len(node.inputs[1].shape) == 3 and \
+           len(node.outputs) == 1 and len(node.outputs[0].shape) == 3, str(node)
+
     b = node.outputs[0].shape[0]
     n = node.outputs[0].shape[1]
     p = node.outputs[0].shape[2]
     m = node.inputs[0].shape[2]
-    return b * n * p * m
+    return b * n * m * p
 
 
 def matmul(node):
-    # float[16, 512], %4: float[512, 512]
     return np.prod(node.inputs[0].shape + [node.inputs[1].shape[-1]])
 
 
@@ -43,8 +51,9 @@ def convolution(node):
     return np.prod(node.outputs[0].shape + node.inputs[1].shape[1:])
 
 
-# TODO: we assume `batch_norm` is fused into `linear` or `conv`; should provide an option to fuse `batch_norm` or not
 def batch_norm(node):
+    # `batch_norm` can be fused into its previous `linear` or `conv` layer
+    # TODO: we should provide an option to not fuse `batch_norm` layer
     return 0
 
 
@@ -80,7 +89,7 @@ handlers = (
     ('aten::adaptive_avg_pool2d', none),
 
     (('aten::add', 'aten::add_', 'aten::cat', 'aten::chunk', 'aten::clone', 'aten::contiguous', 'aten::div',
-      'aten::div_' 'aten::dropout', 'aten::dropout_', 'aten::eq', 'aten::flatten', 'aten::hardtanh_', 'aten::int',
+      'aten::div_', 'aten::dropout', 'aten::dropout_', 'aten::eq', 'aten::flatten', 'aten::hardtanh_', 'aten::int',
       'aten::max_pool1d', 'aten::max_pool2d', 'aten::max_pool3d', 'aten::ne', 'aten::relu', 'aten::relu_',
       'aten::select', 'aten::size', 'aten::slice', 'aten::softmax', 'aten::sum', 'aten::t', 'aten::transpose',
       'aten::view', 'prim::constant', 'prim::listconstruct', 'prim::listunpack', 'prim::numtotensor'), none)
