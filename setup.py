dc6dd4c7fb6911591b3cbbe668592e15f92c64aa
Zhijian Liu <zhijianliu.cs@gmail.com>
2019-09-11 02:04:13 -0400

    [minor] update `setup.py`

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ setup(
     name='torchprofile',
     version=__version__,
     packages=find_packages(exclude=['examples', 'tests']),
-    url='https://github.com/mit-han-lab/torchprofile',
     install_requires=[
         'numpy>=1.14',
         'torch>=1.2',
         'torchvision>=0.4'
-    ]
+    ],
+    url='https://github.com/mit-han-lab/torchprofile/',
+    license='MIT'
 )
