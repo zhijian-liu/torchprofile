e6b16d4dc7a5d46c0701da7a9702d3cbbb26a53a
Zhijian Liu <zhijianliu.cs@gmail.com>
2020-11-20 14:57:09 -0500

    [minor] support `leaky_relu` and some other no-macs operations

--- torchprofile/handlers.py
+++ torchprofile/handlers.py
@@ -86,22 +86,32 @@ def avg_pool_or_mean(node):
     return math.prod(os)
 
 
+def leaky_relu(node):
+    os = node.outputs[0].shape
+    return math.prod(os)
+
+
 handlers = (
-    ('aten::addmm', addmm), ('aten::addmv', addmv), ('aten::bmm', bmm),
-    ('aten::matmul', matmul), (('aten::mul', 'aten::mul_'), mul),
-    ('aten::_convolution', convolution), ('aten::batch_norm', batch_norm),
-    (('aten::instance_norm', 'aten::layer_norm'),
-     instance_norm_or_layer_norm),
+    ('aten::addmm', addmm),
+    ('aten::addmv', addmv),
+    ('aten::bmm', bmm),
+    ('aten::matmul', matmul),
+    (('aten::mul', 'aten::mul_'), mul),
+    ('aten::_convolution', convolution),
+    ('aten::batch_norm', batch_norm),
+    (('aten::instance_norm', 'aten::layer_norm'), instance_norm_or_layer_norm),
     (('aten::adaptive_avg_pool1d', 'aten::adaptive_avg_pool2d',
       'aten::adaptive_avg_pool3d', 'aten::avg_pool1d', 'aten::avg_pool2d',
       'aten::avg_pool3d', 'aten::mean'), avg_pool_or_mean),
+    ('aten::leaky_relu', leaky_relu),
     (('aten::adaptive_max_pool1d', 'aten::adaptive_max_pool2d',
       'aten::adaptive_max_pool3d', 'aten::add', 'aten::add_',
       'aten::alpha_dropout', 'aten::cat', 'aten::chunk', 'aten::clamp',
-      'aten::clone', 'aten::constant_pad_nd', 'aten::contiguous', 'aten::div',
-      'aten::div_', 'aten::dropout', 'aten::dropout_', 'aten::embedding',
-      'aten::eq', 'aten::feature_dropout', 'aten::flatten', 'aten::gt',
-      'aten::hardtanh_', 'aten::int', 'aten::lt', 'aten::log_softmax',
+      'aten::clone', 'aten::constant_pad_nd', 'aten::contiguous',
+      'aten::detach', 'aten::div', 'aten::div_', 'aten::dropout',
+      'aten::dropout_', 'aten::embedding', 'aten::eq', 'aten::feature_dropout',
+      'aten::flatten', 'aten::floor', 'aten::gt', 'aten::hardtanh_',
+      'aten::index', 'aten::int', 'aten::log_softmax', 'aten::lt',
       'aten::max_pool1d', 'aten::max_pool1d_with_indices', 'aten::max_pool2d',
       'aten::max_pool2d_with_indices', 'aten::max_pool3d',
       'aten::max_pool3d_with_indices', 'aten::max_unpool1d',
@@ -111,7 +121,9 @@ handlers = (
       'aten::replication_pad1d', 'aten::replication_pad2d',
       'aten::replication_pad3d', 'aten::rsub', 'aten::select', 'aten::sigmoid',
       'aten::size', 'aten::slice', 'aten::softmax', 'aten::softshrink',
-      'aten::squeeze', 'aten::sub', 'aten::sum', 'aten::t', 'aten::tanh',
-      'aten::threshold', 'aten::transpose', 'aten::view', 'aten::zeros',
+      'aten::squeeze', 'aten::stack', 'aten::sub', 'aten::sum', 'aten::t',
+      'aten::tanh', 'aten::threshold', 'aten::to', 'aten::transpose',
+      'aten::upsample_nearest2d', 'aten::view', 'aten::zeros',
       'prim::constant', 'prim::listconstruct', 'prim::listunpack',
-      'prim::numtotensor', 'prim::tupleconstruct'), None))
+      'prim::numtotensor', 'prim::tupleconstruct'), None),
+)
